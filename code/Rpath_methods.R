95c649e9db2fc056a9b68958066523d80ddc8fb2
Sean Lucey <Sean.Lucey@NOAA.gov>
2015-01-02 13:13:08 -0500

    Added write.Rpath ... note that write is not a generic function in R

--- code/Rpath_methods.R
+++ code/Rpath_methods.R
@@ -1,4 +1,5 @@
 #Functions for Rpath objects
+#Print
 print.Rpath <- function(x, rows = NA, morts = F, ...){
   cat("Rpath model:\n")
   if(max(x$EE, na.rm = T) > 1){
@@ -51,7 +52,7 @@ print.Rpath <- function(x, rows = NA, morts = F, ...){
   if(is.na(rows)) print(out, nrows = Inf) else print(out, topn = rows)
 }
 
-
+#Summary
 summary.Rpath <- function(object, ...){
   x <- object
   cat("Rpath model:\n")
@@ -73,4 +74,55 @@ summary.Rpath <- function(object, ...){
   print(table)
   cat("\nRpath model also includes:\n")
   print(names(GOA))
+}
+
+
+write.test <- function(x, file = '', ...){
+  write.csv(x, file = file)
+}
+
+#Write -- note, not a generic function
+write.Rpath <- function(x, file, morts = F, ...){
+  if(morts == F){
+    removals <- rowSums(x$Catch) + rowSums(x$Discard)
+    out <- data.table(Group    = x$Group,
+                      type     = x$type,
+                      TL       = x$TL,
+                      Biomass  = x$BB,
+                      PB       = x$PB,
+                      QB       = x$QB,
+                      EE       = x$EE,
+                      GE       = x$GE,
+                      Removals = removals)
+  }
+  if(morts == T){
+    ngroup <- x$NUM_LIVING + x$NUM_DEAD
+    out <- data.table(Group    = x$Group[1:ngroup],
+                      type     = x$type [1:ngroup],
+                      PB       = x$PB   [1:ngroup])
+    #Calculate M0
+    M0  <- c(x$PB[1:x$NUM_LIVING] * (1 - x$EE[1:x$NUM_LIVING]), 
+             x$EE[(x$NUM_LIVING + 1):ngroup])
+    out <- cbind(out, M0)
+    #Calculate F mortality
+    totcatch <- x$Catch + x$Discards
+    Fmort    <- as.data.table(totcatch / x$BB[row(as.matrix(totcatch))])
+    setnames(Fmort, paste('V',  1:x$NUM_GEARS,                     sep = ''), 
+             paste('F.', x$Group[(ngroup +1):x$NUM_GROUPS], sep = ''))
+    out  <- cbind(out, Fmort[1:ngroup, ])
+    #Calculate M2
+    bio  <- x$BB[1:x$NUM_LIVING]
+    BQB  <- bio * x$QB[1:x$NUM_LIVING]
+    diet <- as.data.table(x$DC)
+    nodetrdiet <- diet[1:x$NUM_LIVING, ]
+    detrdiet   <- diet[(x$NUM_LIVING +1):ngroup, ]
+    newcons    <- nodetrdiet * BQB[col(as.matrix(nodetrdiet))]
+    predM      <- newcons / bio[row(as.matrix(newcons))]
+    detcons    <- detrdiet * BQB[col(as.matrix(detrdiet))]
+    predM      <- rbind(predM, detcons)
+    setnames(predM, paste('V',  1:x$NUM_LIVING,    sep = ''), 
+             paste('M2.', x$Group[1:x$NUM_LIVING], sep = ''))
+    out <- cbind(out, predM)
+  }
+  write.csv(out, file = file)
 }
