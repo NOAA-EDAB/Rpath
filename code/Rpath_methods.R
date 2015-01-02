cd72605efa24d27fc6e7f027462447c92f07c27a
Sean Lucey <Sean.Lucey@NOAA.gov>
2015-01-02 11:14:56 -0500

    Updated print.Rpath to include morts output and updated summary.Rpath to include names of other items in the list

--- code/Rpath_methods.R
+++ code/Rpath_methods.R
@@ -1,5 +1,5 @@
 #Functions for Rpath objects
-print.Rpath <- function(x, ...){
+print.Rpath <- function(x, rows = NA, morts = F, ...){
   cat("Rpath model:\n")
   if(max(x$EE, na.rm = T) > 1){
     unbalanced.groups <- x$Group[which(EE > 1)]
@@ -7,38 +7,70 @@ print.Rpath <- function(x, ...){
   } else {
     cat("Balanced\n")
   }
-  removals <- rowSums(x$Catch) + rowSums(x$Discard)
-  out <- data.table(Group    = x$Group,
-                    type     = x$type,
-                    TL       = x$TL,
-                    Biomass  = x$BB,
-                    PB       = x$PB,
-                    QB       = x$QB,
-                    EE       = x$EE,
-                    GE       = x$GE,
-                    Removals = removals)
-  print(out, nrow = Inf)
+  if(morts == F){
+    removals <- rowSums(x$Catch) + rowSums(x$Discard)
+    out <- data.table(Group    = x$Group,
+                      type     = x$type,
+                      TL       = x$TL,
+                      Biomass  = x$BB,
+                      PB       = x$PB,
+                      QB       = x$QB,
+                      EE       = x$EE,
+                      GE       = x$GE,
+                      Removals = removals)
+  }
+  if(morts == T){
+    ngroup <- x$NUM_LIVING + x$NUM_DEAD
+    out <- data.table(Group    = x$Group[1:ngroup],
+                      type     = x$type [1:ngroup],
+                      PB       = x$PB   [1:ngroup])
+    #Calculate M0
+    M0  <- c(x$PB[1:x$NUM_LIVING] * (1 - x$EE[1:x$NUM_LIVING]), 
+             x$EE[(x$NUM_LIVING + 1):ngroup])
+    out <- cbind(out, M0)
+    #Calculate F mortality
+    totcatch <- x$Catch + x$Discards
+    Fmort    <- as.data.table(totcatch / x$BB[row(as.matrix(totcatch))])
+    setnames(Fmort, paste('V',  1:x$NUM_GEARS,                     sep = ''), 
+                    paste('F.', x$Group[(ngroup +1):x$NUM_GROUPS], sep = ''))
+    out  <- cbind(out, Fmort[1:ngroup, ])
+    #Calculate M2
+    bio  <- x$BB[1:x$NUM_LIVING]
+    BQB  <- bio * x$QB[1:x$NUM_LIVING]
+    diet <- as.data.table(x$DC)
+    nodetrdiet <- diet[1:x$NUM_LIVING, ]
+    detrdiet   <- diet[(x$NUM_LIVING +1):ngroup, ]
+    newcons    <- nodetrdiet * BQB[col(as.matrix(nodetrdiet))]
+    predM      <- newcons / bio[row(as.matrix(newcons))]
+    detcons    <- detrdiet * BQB[col(as.matrix(detrdiet))]
+    predM      <- rbind(predM, detcons)
+    setnames(predM, paste('V',  1:x$NUM_LIVING,    sep = ''), 
+             paste('M2.', x$Group[1:x$NUM_LIVING], sep = ''))
+    out <- cbind(out, predM)
+  }
+  if(is.na(rows)) print(out, nrows = Inf) else print(out, topn = rows)
 }
 
 
 summary.Rpath <- function(object, ...){
-  z <- object
-  EE <- z$EE
-  if(max(EE, na.rm = T) > 1){
-    balance <- "Unbalanced"
-    unbalanced.groups <- z$Group[which(EE > 1)]
-  }else{
-    balance <- "Balanced"
+  x <- object
+  cat("Rpath model:\n")
+  if(max(x$EE, na.rm = T) > 1){
+    unbalanced.groups <- x$Group[which(EE > 1)]
+    print(paste("Unbalanced:\n", unbalanced.groups))
+  } else {
+    cat("Balanced\n")
   }
-  totbiomass <- sum(z$BB,    na.rm = T)
-  totcatch   <- sum(z$Catch, na.rm = T)
-  table <- data.table(Num.Groups   = z$NUM_GROUPS,
-                      Num.Living   = z$NUM_LIVING,
-                      Num.Detritus = z$NUM_DEAD,
-                      Num.Fleets   = z$NUM_GEARS,
+  cat("\nSummary Statistics:\n")
+  totbiomass <- sum(x$BB,    na.rm = T)
+  totcatch   <- sum(x$Catch, na.rm = T)
+  table <- data.table(Num.Groups   = x$NUM_GROUPS,
+                      Num.Living   = x$NUM_LIVING,
+                      Num.Detritus = x$NUM_DEAD,
+                      Num.Fleets   = x$NUM_GEARS,
                       TotBiomass   = totbiomass,
                       TotCatch     = totcatch)
-  ans <- list(model = balance,
-              stats = table)
-  return(ans)
+  print(table)
+  cat("\nRpath model also includes:\n")
+  print(names(GOA))
 }
