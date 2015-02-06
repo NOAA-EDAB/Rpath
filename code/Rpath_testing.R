0799f675478d31af5ad36043b3bf4a4695a148f0
Sean Lucey <Sean.Lucey@NOAA.gov>
2015-02-06 15:47:44 -0500

    Pushing to git hub to use on laptop

--- code/Rpath_testing.R
+++ code/Rpath_testing.R
@@ -5,7 +5,7 @@
 
 #User parameters
 
-windows <- T
+windows <- F
 if(windows == T){
   r.dir    <- "C:\\Users\\Sean.Lucey\\Desktop\\Rpath_git\\Rpath\\code\\"
   data.dir <- "C:\\Users\\Sean.Lucey\\Desktop\\Rpath_git\\Rpath\\data\\"
@@ -58,148 +58,10 @@ summary(GOA)
 
 #Ecosim
 library(Rcpp)
-
-GOA.sim <- ecosim_init(GOA, YEARS = 100, juvfile)
-
 sourceCpp(paste(r.dir, 'test_ecosim.cpp', sep = ''))
 
-deriv_master(GOA.sim, 0, 0, 0)
-
-#test pointers
-test <- list(x = data.frame(x = 1:10, y = rep(2, 10)), z = rep(0, 10))
-
-cppFunction('NumericMatrix RmatT (DataFrame x){
-              //Tranposes the column and rows
-              int nc = x.nrows();
-              int nr = x.size();
-              NumericMatrix y(Dimension(nr, nc));
-              
-              for( int i = 0; i < x.size(); i++){
-                NumericVector xcol = x[i];
-                NumericMatrix::Row yy = y( i, _);
-                yy = xcol;
-              }
-              
-              return y;
-            }')
-
-cppFunction('NumericVector cpptest(List test){
-            DataFrame x = as<DataFrame>(test["x"]);
-            NumericVector out = x[0];
-            
-out[0] = 15;
-            Rcout << "Test" << std::endl;
-            return out;
-            }')
-
-
-cpptest(test)
-test
-cpptest(test)
-
-
-
-
-
-cppFunction('NumericMatrix DFtoA (DataFrame x){
-              int nr = x.nrows();
-              int nc = x.size();
-              NumericMatrix y(Dimension(nr, nc));
-              for( int i = 0; i < x.size(); i++){
-                NumericVector xcol = x[i];
-                NumericMatrix::Column yy = y( _, i);
-                yy = xcol;
-              }
-              return y;
-              }')
-
-cppFunction('NumericMatrix RmatT (DataFrame x){
-              //Tranposes the column and rows
-              int nr = x.nrows();
-              int nc = x.size();
-              NumericMatrix y(Dimension(nc, nr));
-              for( int i = 0; i < x.size(); i++){
-                NumericVector xcol = x[i];
-                NumericMatrix::Row yy = y( i, _);
-                yy = xcol;
-              }
-              return y;
-              }')
-
-cppFunction('List cppTest(List mod){
-            #define STEPS_PER_YEAR 12
-            NumericVector Q;
-            int NumPredPreyLinks           = as<int>(mod["NumPredPreyLinks"]);
-            NumericVector preyYY           = as<NumericVector>(mod["preyYY"]);
-            NumericVector PreyFrom         = as<NumericVector>(mod["PreyFrom"]);
-            NumericVector PreyTo           = as<NumericVector>(mod["PreyTo"]);
-            NumericVector HandleSwitch     = as<NumericVector>(mod["HandleSwitch"]);
-            NumericVector COUPLED          = as<NumericVector>(mod["COUPLED"]);
-
-            for (int links=1; links<=NumPredPreyLinks; links++){
-   	          int prey = PreyFrom[links];
- 		          int pred = PreyTo[links];
-              Q[links] = pow(preyYY[prey], COUPLED * HandleSwitch[links]);
-            }
-            List out = List::create(Named("predYY")         = predYY,
-                                    Named("Q")              = Q);
-            return out;
-            }')
-
-cppFunction('int deriv_master(List mod, int y, int m, int d){
-
-            int STEPS_PER_YEAR = 12;
-            int sp, i;
-
-            // Parse out List mod
-            int NUM_LIVING                 = as<int>(mod["NUM_LIVING"]);
-            int NUM_DEAD                   = as<int>(mod["NUM_DEAD"]);
-            int juv_N                      = as<int>(mod["juv_N"]);
-            NumericVector spnum            = as<NumericVector>(mod["spnum"]);
-            NumericVector B_BaseRef        = as<NumericVector>(mod["B_BaseRef"]);
-            NumericVector state_BB         = as<NumericVector>(mod["state_BB"]);
-            NumericVector state_Ftime      = as<NumericVector>(mod["state_Ftime"]);
-            NumericVector stanzaPred       = as<NumericVector>(mod["stanzaPred"]);
-            NumericVector stanzaBasePred   = as<NumericVector>(mod["stanzaBasePred"]);
-            NumericVector JuvNum           = as<NumericVector>(mod["JuvNum"]);
-            NumericVector AduNum           = as<NumericVector>(mod["AduNum"]);
-            NumericVector preyYY           = as<NumericVector>(mod["preyYY"]);
-            NumericVector predYY           = as<NumericVector>(mod["predYY"]);
-            DataFrame force_bysearch       = as<DataFrame>(mod["force_bysearch"]);
-
-            
-            //  Set YY = B/B(ref) for functional response; note that foraging time is
-            //  used here as a biomass modifier before the main functional response  
-            for (sp = 1; sp <= NUM_LIVING + NUM_DEAD; sp++){
-              preyYY[sp] = state_Ftime[sp] * state_BB[sp] / B_BaseRef[sp];
-              predYY[sp] = state_Ftime[sp] * state_BB[sp] / B_BaseRef[sp];
-              }    
-            // The sun always has a biomass of 1 for Primary Production
-            preyYY[0] = 1.0;  predYY[0] = 1.0;
-            
-            // Set functional response biomass for juvenile and adult groups (including foraging time) 
-            for (i=1; i<=juv_N; i++){
-              if (stanzaBasePred[JuvNum[i]]>0){
-                predYY[JuvNum[i]] = state_Ftime[JuvNum[i]] * 
-                stanzaPred[JuvNum[i]]/stanzaBasePred[JuvNum[i]];
-                predYY[AduNum[i]] = state_Ftime[AduNum[i]] * 
-                stanzaPred[AduNum[i]]/stanzaBasePred[AduNum[i]];
-              }
-            }
-            // add "mediation by search rate" KYA 7/8/08
-            for (sp=1; sp<=NUM_LIVING+NUM_DEAD; sp++){
-              NumericVector sp_force_bysearch = force_bysearch[sp];
-              predYY[sp] *= sp_force_bysearch[y*STEPS_PER_YEAR+m]; 
-            }
-            }')
-
-deriv_master(GOA.sim, 0, 0, 0)
-
-
-
-
-test <- cpptest(GOA.sim)
-
+GOA.sim <- ecosim_init(GOA, YEARS = 100, juvfile)
+GOA.sim <- ecosim_run(GOA.sim, 0, 100)
 
 
 
@@ -237,4 +99,84 @@ base_sim <- load_ecosim( Years = 100, BaseFile, DietFile, PedFile, JuvFile)
 run0 <- ecosim_run(base_sim,0,100)  
 
 # resulting output biomass
-summary(run0$out_BB)
+summary(run0$out_BB[34])
+summary(GOA.sim$out_BB[34])
+
+plot(GOA.sim$out_BB[34])
+
+cppFunction('int ecosim_test(List mod, int StartYear, int EndYear){
+            int y, m, c, j, dd;
+            int sp, t, i, ageMo, s, link,prey,pred,links,gr;
+            int inbound;
+            double old_B, new_B, nn, ww, bb, pd;
+            double newbio; float ystep;
+            double bioanom, bioratio;
+            unsigned int LL;
+            int STEPS_PER_YEAR = 12;
+
+            // Parse out List mod
+            int NUM_LIVING = as<int>(mod["NUM_LIVING"]);
+            int NUM_DEAD   = as<int>(mod["NUM_DEAD"]);
+            int juv_N      = as<int>(mod["juv_N"]);
+            int init_run   = as<int>(mod["init_run"]);
+            int BURN_YEARS = as<int>(mod["BURN_YEARS"]);
+            int CRASH_YEAR = as<int>(mod["CRASH_YEAR"]);
+            
+            NumericVector B_BaseRef        = as<NumericVector>(mod["B_BaseRef"]);
+            NumericVector NoIntegrate      = as<NumericVector>(mod["NoIntegrate"]);
+            NumericVector state_BB         = as<NumericVector>(mod["state_BB"]);
+            NumericVector state_Ftime      = as<NumericVector>(mod["state_Ftime"]);
+            NumericVector FtimeAdj         = as<NumericVector>(mod["FtimeAdj"]);
+            NumericVector FtimeQBOpt       = as<NumericVector>(mod["FtimeQBOpt"]);
+            NumericMatrix WageS            = as<NumericMatrix>(mod["WageS"]);
+            NumericMatrix NageS            = as<NumericMatrix>(mod["NageS"]);
+            NumericVector stanzaPred       = as<NumericVector>(mod["stanzaPred"]);
+            NumericVector SpawnBio         = as<NumericVector>(mod["SpawnBio"]);
+            NumericVector JuvNum           = as<NumericVector>(mod["JuvNum"]);
+            NumericVector AduNum           = as<NumericVector>(mod["AduNum"]);
+            NumericVector firstMoAdu       = as<NumericVector>(mod["firstMoAdu"]);
+            NumericVector DerivT           = as<NumericVector>(mod["DerivT"]);
+            NumericVector dyt              = as<NumericVector>(mod["dyt"]);
+            NumericVector biomeq           = as<NumericVector>(mod["biomeq"]);
+            NumericVector FoodGain         = as<NumericVector>(mod["FoodGain"]);
+            NumericVector FishingLoss      = as<NumericVector>(mod["FishingLoss"]);
+            
+            DataFrame out_BB           = as<DataFrame>(mod["out_BB"]);
+            DataFrame out_CC           = as<DataFrame>(mod["out_CC"]);
+            DataFrame out_SSB          = as<DataFrame>(mod["out_SSB"]);
+            DataFrame out_rec          = as<DataFrame>(mod["out_rec"]);
+            
+            for (y = StartYear; y < EndYear; y++){                                  
+            for (m = 0; m < STEPS_PER_YEAR; m++){
+                     
+           // dd is index for saving monthly output
+  			      dd = y * STEPS_PER_YEAR + m;
+           
+           // save current state to output matrix
+           // For non-split pools, SSB is output output as the same as B.
+					 // For adult split pools, it is overwritten with "actual" SSB, 
+					 // for juvs it is 0. 
+              for (sp = 1; sp <= NUM_LIVING + NUM_DEAD; sp++){ 
+                  NumericVector sp_out_BB  = out_BB[sp];
+                  NumericVector sp_out_SSB = out_SSB[sp];
+                  NumericVector sp_out_rec = out_rec[sp];
+                  
+                  sp_out_BB[dd]  = state_BB[sp];
+                  sp_out_SSB[dd] = state_BB[sp];
+                  sp_out_rec[dd] = state_BB[sp];
+              }
+              for (i = 1; i <= juv_N; i++){
+                  NumericVector jv_out_SSB = out_SSB[JuvNum[i]];
+                  NumericVector ad_out_SSB = out_SSB[AduNum[i]];
+                  NumericVector ad_out_rec = out_rec[AduNum[i]];
+                  
+                  jv_out_SSB[dd] = 0.0;
+									ad_out_SSB[dd] = SpawnBio[i];
+									ad_out_rec[dd] = NageS(firstMoAdu[i], i) * WageS(firstMoAdu[i], i);
+              }
+}
+}
+       return(0);
+            }')
+
+ecosim_test(GOA.sim, 0, 50)
