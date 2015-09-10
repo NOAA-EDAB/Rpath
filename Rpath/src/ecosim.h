f390b439e39d9612b91efc87156c5b00b6bc2542
kaydin <kerim@u.washington.edu>
2015-09-10 15:50:46 -0700

    Commented ecosim.h

--- Rpath/src/ecosim.h
+++ Rpath/src/ecosim.h
@@ -2,32 +2,31 @@
 using namespace Rcpp;
  
 // Various Constants
-//#define STEPS_PER_MONTH    1        // Integration Steps per month (speed (5) vs. accuracy (10 to 30))
-#define SPM                1.0      // Double version of same 
 #define MAX_MONTHS_STANZA  400      // Max number age pools within stanza calcs (months)
 #define LO_DISCARD         1e-4     // Discard Limit for sense
 #define HI_DISCARD         1e+4     // Discard Limit for sense
-
-// Some fixed definitions, shouldn't need to tweak
 #define STEPS_PER_YEAR     12                   // Steps per year between stanzas ("months")
-#define MIN_THRESHOLD(x,y) if ((x)<(y))(x)=(y)  // Set x to min y if too small
-#define MAX_THRESHOLD(x,y) if ((x)>(y))(x)=(y)  // Set x to max y if too big
-#define DELTA_T            0.083333333333333333
+#define DELTA_T            0.083333333333333333 // 1/12 for steps per month
 #define SORWT              0.5                  // Fast Equilibrium smoother
 #define EPSILON            1E-8                 // Test threshold for "too close to zero"
 #define BIGNUM             1E+8                 // Test threshold for "too big"
 
-List deriv_vector(List params, List state, List forcing, List fishing, int y, int m, double tt);
-List Adams_run(List params, List instate, List forcing, List fishing, int StartYear, int EndYear);
-List rk4_run(List params, List instate, List forcing, List fishing, int StartYear, int EndYear);
+#define MIN_THRESHOLD(x,y) if ((x)<(y))(x)=(y)  // Set x to min y if too small
+#define MAX_THRESHOLD(x,y) if ((x)>(y))(x)=(y)  // Set x to max y if too big
 
-int deriv_old(List mod, int y, int m, int d);
-int SplitSetPred(List mod);
-int update_stanzas(List mod, int yr, int mon);
-int Adams_Basforth_old (List mod, int StartYear, int EndYear);
+// Functions in ecosim.cpp 
+   List deriv_vector(List params, List state, List forcing, List fishing, int y, int m, double tt);
+   List Adams_run(List params, List instate, List forcing, List fishing, int StartYear, int EndYear);
+   List rk4_run(List params, List instate, List forcing, List fishing, int StartYear, int EndYear);
 
-NumericVector vpow(const NumericVector base, const NumericVector exp) {
-  NumericVector out(base.size());
-  std::transform(base.begin(), base.end(),
+   int deriv_old(List mod, int y, int m, int d);
+   int SplitSetPred(List mod);
+   int update_stanzas(List mod, int yr, int mon);
+   int Adams_Basforth_old (List mod, int StartYear, int EndYear);
+
+// Power function that works with NumericVectors
+   NumericVector vpow(const NumericVector base, const NumericVector exp) {
+   NumericVector out(base.size());
+   std::transform(base.begin(), base.end(),
                  exp.begin(), out.begin(), ::pow); return out; }
          
