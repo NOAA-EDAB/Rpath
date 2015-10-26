979a3c36edb3e03f78fe11603920129993ac94b0
Sean Lucey <Sean.Lucey@NOAA.gov>
2015-10-26 16:29:01 -0400

    Added multistanza code to REco and check.param

--- code/REco.R
+++ code/REco.R
@@ -30,9 +30,6 @@ groups <- c('Seabirds', 'Whales', 'Seals', 'JuvRoundfish1', 'AduRoundfish1',
             'Macrobenthos', 'Zooplankton', 'Phytoplankton', 'Detritus', 
             'Discards', 'Trawlers', 'Midwater', 'Dredgers')
 
-stanzas <- c(rep(NA, 3), rep('Roundfish1', 2), rep('Roundfish2', 2), 
-             rep('Flatfish1', 2), rep ('Flatfish2', 2), rep(NA, 14))
-
 types  <- c(rep(0, 19), 1, rep(2, 2), rep(3, 3))
 
 modfile <- create.rpath.param(parameter = 'model', group = groups, type = types)
@@ -41,7 +38,53 @@ modfile <- create.rpath.param(parameter = 'model', group = groups, type = types)
 biomass <- c(0.0149, 0.454, NA, NA, 1.39, NA, 5.553, NA, 5.766, NA,
              0.739, 7.4, 5.1, 4.7, 5.1, NA, 7, 17.4, 23, 10, rep(NA, 5))
 
+#EE's
+modfile[Group %in% c('Seals', 'Megabenthos'), EE := 0.8]
+
+#PB and QB
+pb <- c(0.098, 0.031, 0.100, 2.026, 0.42, 2.1, 0.425, 1.5, 0.26, 1.1, 0.18, 0.6,
+        0.61, 0.65, 1.5, 0.9, 1.3, 7, 39, 240, rep(NA, 5))
+
+qb <- c(76.750, 6.976, 34.455, NA, 2.19, NA, 3.78, NA, 1.44, NA, 1.69,
+        1.764, 3.52, 5.65, 3.6, 2.984, rep (NA, 9))
+
 modfile <- modfile[, Biomass := biomass]
+modfile <- modfile[, PB := pb]
+modfile <- modfile[, QB := qb]
+
+#Production to Consumption for those groups without a QB
+modfile <- modfile[Group %in% c('Shellfish', 'Zooplankton'), ProdCons:= 0.25]
+modfile <- modfile[Group == 'Macrobenthos', ProdCons := 0.35]
+
+#Biomass accumulation and unassimilated production
+modfile <- modfile[, BioAcc  := c(rep(0, 22), rep(NA, 3))]
+modfile <- modfile[, Unassim := c(rep(0.2, 18), 0.4, rep(0, 3), rep(NA, 3))]
+
+#Detrital Fate
+modfile <- modfile[, Detritus := c(rep(1, 20), rep(0, 5))]
+modfile <- modfile[, Discards := c(rep(0, 22), rep(1, 3))]
+
+#Fisheries
+#Landings
+trawl  <- c(rep(0, 4), 0.08, 0, 0.32, 0, 0.09, 0, 0.05, 0.2, rep(0, 10), rep(NA, 3))
+mid    <- c(rep(0, 12), 0.3, 0.08, 0.02, rep(0, 7), rep(NA, 3))
+dredge <- c(rep(0, 15), 0.1, 0.5, rep(0, 5), rep(NA, 3))
+modfile <- modfile[, Trawlers := trawl]
+modfile <- modfile[, Midwater := mid]
+modfile <- modfile[, Dredgers := dredge]
+
+#Discards
+trawl.d  <- c(1e-5, 1e-7, 0.001, 0.001, 0.005, 0.001, 0.009, 0.001, 0.04, 0.001,
+              0.01, 0.08, 0.001, 0.001, 0.001, rep(0, 7), rep(NA, 3))
+mid.d    <- c(rep(0, 2), 0.001, 0.001, 0.01, 0.001, 0.01, rep(0, 4), 0.05, 0.05,
+              0.01, 0.01, rep(0, 7), rep(NA, 3))
+dredge.d <- c(rep(0, 3), 0.001, 0.05, 0.001, 0.05, 0.001, 0.05, 0.001, 0.01, 0.05,
+              rep(0, 3), 0.09, 0.01, 1e-4, rep(0, 4), rep(NA, 3))
+modfile <- modfile[, Trawlers.disc := trawl.d]
+modfile <- modfile[, Midwater.disc := mid.d]
+modfile <- modfile[, Dredgers.disc := dredge.d]
+
+
 
 #Calculate the multistanza biomass/consumption
 groups <- data.table(StGroupNum  = 1:4,
@@ -65,7 +108,17 @@ stanzas <- data.table(StGroupNum = c(1, 1, 2, 2, 3, 3, 4, 4),
 
 
 
-rpath.stanzas(modfile, groups, stanzas)
+stanza.to.plot <- rpath.stanzas(modfile, groups, stanzas, output = T)
+
+#Plot multistanza plots
+stanzaplot(stanza.to.plot, stanzas, 1)
+stanzaplot(stanza.to.plot, stanzas, 2)
+stanzaplot(stanza.to.plot, stanzas, 3)
+stanzaplot(stanza.to.plot, stanzas, 4)
+
+#Check model parameter file
+check.rpath.param(modfile, type = 'model')
+
 
 
 
