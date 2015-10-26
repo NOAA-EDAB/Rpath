e68a153f7f39f321ef9c41167f06729d710d7045
Sean Lucey <Sean.Lucey@NOAA.gov>
2015-10-26 16:28:26 -0400

    Fixed missing bracket error and added a check that fleets are not catching other fleets

--- Rpath/R/param.R
+++ Rpath/R/param.R
@@ -113,7 +113,8 @@ if(!is.na(filename)){
 #'
 #'@family Rpath functions
 #'
-#'@param filename Name of the parameter file.  Must be a .csv. 
+#'@param filename Name of the parameter file.  Can be the path to a .csv or an R
+#'object. 
 #'@param type The type of parameter file you are checking.  Choices include "model",
 #'  "diet", "juvenile", and "pedigree".
 #'
@@ -123,7 +124,11 @@ if(!is.na(filename)){
 #'@import data.table
 #'@export
 check.rpath.param <- function(filename, type = 'model'){
-  parameter <- as.data.table(read.csv(filename))
+  if(is.character(filename)){
+    parameter <- as.data.table(read.csv(filename))
+  } else {
+    parameter <- as.data.table(filename)
+  }
   
   if(type == 'model'){
     #Check to make sure all types are represented
@@ -137,115 +142,139 @@ check.rpath.param <- function(filename, type = 'model'){
     n.dead   <- length(parameter[Type == 2, Group])
     n.fleet  <- length(parameter[Type == 3, Group])
     if(ncol(parameter) != 10 + n.dead + 2 * n.fleet){
-      stop('Model does not have the correct number of column.  There should be 10 columns plus one for 
-           each detrital group plus two for each fleet group (landings and discards).  Please double check 
-           your columns')
+      stop('Model does not have the correct number of column.  There should be 10 
+           columns plus one for each detrital group plus two for each fleet group 
+           (landings and discards).  Please double check your columns')
+    }
       
     #Check that either biomass or EE is entered and not both
     if(length(parameter[is.na(Biomass) & is.na(EE) & Type < 2, Group]) > 0){
       stop(paste(parameter[is.na(Biomass) & is.na(EE) & Type < 2, Group], 
-                 'are missing both Biomass and EE...must enter one', sep = ' '))
+                 'are missing both Biomass and EE...must enter one \n', sep = ' '))
     }
     if(length(parameter[!is.na(Biomass) & !is.na(EE) & Type < 2, Group]) > 0){
       stop(paste(parameter[!is.na(Biomass) & !is.na(EE) & Type < 2, Group],
-                 'have both Biomass and EE...only one should be entered', sep = ' '))
+                 'have both Biomass and EE...only one should be entered \n', sep = ' '))
       #consider making this a warning() since you can have both in EwE
     }
   
     #Check that Biomass / PB / QB / EE / ProdCons is not entered for types 2 and 3
     if(length(parameter[Type > 1 & !is.na(Biomass), Group]) > 0){
       stop(paste(parameter[Type > 1 & !is.na(Biomass), Group], 
-                 'are not living and should not have a biomass...set to NA', sep = ' '))
+                 'are not living and should not have a biomass...set to NA \n', 
+                 sep = ' '))
     }
     if(length(parameter[Type > 1 & !is.na(PB), Group]) > 0){
       stop(paste(parameter[Type > 1 & !is.na(PB), Group], 
-                 'are not living and should not have a PB...set to NA', sep = ' '))
+                 'are not living and should not have a PB...set to NA \n', sep = ' '))
     }
     if(length(parameter[Type > 1 & !is.na(QB), Group]) > 0){
       stop(paste(parameter[Type > 1 & !is.na(QB), Group], 
-                 'are not living and should not have a QB...set to NA', sep = ' '))
+                 'are not living and should not have a QB...set to NA \n', sep = ' '))
     }
     if(length(parameter[Type > 1 & !is.na(EE), Group]) > 0){
       stop(paste(parameter[Type > 1 & !is.na(EE), Group], 
-                 'are not living and should not have a EE...set to NA', sep = ' '))
+                 'are not living and should not have a EE...set to NA \n', sep = ' '))
     }
     if(length(parameter[Type > 1 & !is.na(ProdCons), Group]) > 0){
       stop(paste(parameter[Type > 1 & !is.na(ProdCons), Group], 
-                 'are not living and should not have a ProdCons...set to NA', sep = ' '))
+                 'are not living and should not have a ProdCons...set to NA \n', 
+                 sep = ' '))
     }
     
     #Check that types 0 and 1 have a PB
     if(length(parameter[Type < 2 & is.na(PB), Group]) > 0){
       stop(paste(parameter[Type < 2 & is.na(PB), Group],
-                 'are missing a PB...set to >= 0', sep = ' '))
+                 'are missing a PB...set to >= 0 \n', sep = ' '))
     }
     
     #Check that consumers have a QB or ProdCons but not both
     if(length(parameter[is.na(QB) & is.na(ProdCons) & Type == 0, Group]) > 0){
       stop(paste(parameter[is.na(QB) & is.na(ProdCons) & Type == 0, Group], 
-                 'are missing both QB and ProdCons...must enter one', sep = ' '))
+                 'are missing both QB and ProdCons...must enter one \n', sep = ' '))
     }
     if(length(parameter[!is.na(QB) & !is.na(ProdCons) & Type == 0, Group]) > 0){
       stop(paste(parameter[!is.na(QB) & !is.na(ProdCons) & Type == 0, Group],
-                 'have both QB and ProdCons...only one should be entered', sep = ' '))
+                 'have both QB and ProdCons...only one should be entered \n', 
+                 sep = ' '))
     }
     
     #Check that BioAcc / Unassim is NA for fleets and numeric for types < 3
     if(length(parameter[Type == 3 & !is.na(BioAcc), Group]) > 0){
       stop(paste(parameter[Type == 3 & !is.na(BioAcc), Group],
-                 'are fleets and should not have a BioAcc...set to NA', sep = ' '))
+                 'are fleets and should not have a BioAcc...set to NA \n', sep = ' '))
     }
     if(length(parameter[Type == 3 & !is.na(Unassim), Group]) > 0){
       stop(paste(parameter[Type == 3 & !is.na(Unassim), Group],
-                 'are fleets and should not have an Unassim...set to NA', sep = ' '))
+                 'are fleets and should not have an Unassim...set to NA \n', sep = ' '))
     }
     if(length(parameter[Type != 3 & is.na(BioAcc), Group]) > 0){
       stop(paste(parameter[Type != 3 & is.na(BioAcc), Group],
-                 'must have a number for BioAcc...set to >= 0', sep = ' '))
+                 'must have a number for BioAcc...set to >= 0 \n', sep = ' '))
     }
     if(length(parameter[Type != 3 & is.na(Unassim), Group]) > 0){
       stop(paste(parameter[Type != 3 & is.na(Unassim), Group],
-                 'must have a number for Unassim...set to >= 0', sep = ' '))
+                 'must have a number for Unassim...set to >= 0 \n', sep = ' '))
     }
     
     #Check that only Type 2 has DetInput set
     if(length(parameter[Type != 2 & !is.na(DetInput), Group]) > 0){
       stop(paste(parameter[Type != 2 & !is.na(DetInput), Group],
-                 'are not detritus...set DetInput to NA', sep = ' '))
+                 'are not detritus...set DetInput to NA \n', sep = ' '))
     }
     if(length(parameter[Type == 2 & is.na(DetInput), Group]) >0){
       stop(paste(parameter[Type == 2 & is.na(DetInput), Group],
-                 'are detritus...set DetInput to 0', sep = ' '))
+                 'are detritus...set DetInput to 0 \n', sep = ' '))
     }
     
     #Check detritus fate is numeric and sum to 1
     det.matrix <- parameter[, 11:(10 + n.dead), with = F]
     test.rows  <- rowSums(det.matrix)
     if(length(setdiff(which(parameter[, Type] == 2), which(test.rows != 1))) > 0){
-      stop(paste(parameter[, Group][setdiff(which(parameter[, Type] == 2), which(test.rows != 1))],
-                 'detrital fate does not sum to 1', sep = ' '))
+      stop(paste(parameter[, Group][setdiff(which(parameter[, Type] == 2), 
+                                            which(test.rows != 1))],
+                 'detrital fate does not sum to 1 \n', sep = ' '))
     }
     if(length(which(is.na(det.matrix))) > 0){
       na.group <- which(is.na(det.matrix))
-      for(i in 1:length(na.group)) while(na.group[i] > n.groups) na.group[i] <- na.group[i] - n.groups
+      for(i in 1:length(na.group)) while(na.group[i] > n.groups) na.group[i] <- 
+        na.group[i] - n.groups
       na.group <- unique(na.group)
       stop(paste(parameter[na.group, Group], 
-                 'one or more detrital fates are NA...set to >= 0', sep = ' '))
+                 'one or more detrital fates are NA...set to >= 0 \n', sep = ' '))
     }
     
     #Check that landings and discards are numbers for type < 3
-    fleet.matrix <- parameter[, (11 + n.dead):ncol(parameter), with = F]
+    fleet.matrix <- parameter[1:(n.groups - n.fleet), (11 + n.dead):ncol(parameter), 
+                              with = F]
     if(length(which(is.na(fleet.matrix))) > 0){
       na.group <- which(is.na(fleet.matrix))
-      for(i in 1:length(na.group)) while(na.group[i] > n.groups) na.group[i] <- na.group[i] - n.groups
+      for(i in 1:length(na.group)) while(na.group[i] > n.groups) na.group[i] <- 
+        na.group[i] - n.groups
       na.group <- unique(na.group)
       stop(paste(parameter[na.group, Group], 
-                 'one or more detrital fates are NA...set to >= 0', sep = ' '))
+                 'one or more detrital fates are NA...set to >= 0 \n', sep = ' '))
     }
-  #Need to make sure only non-fleets counted then do again for only NAs in fleets.....
     
+    #Check that fleets aren't catching other fleets
+    fleet.matrix.2 <- parameter[(n.groups - n.fleet + 1):n.groups, (11 + n.dead):
+                                  ncol(parameter), with = F]
+    if(length(which(!is.na(fleet.matrix.2))) > 0){
+      not.na.group <- which(!is.na(fleet.matrix.2))
+      for(i in 1:length(not.na.group)){
+        while(not.na.group[i] > n.fleet){
+          not.na.group[i] <- not.na.group[i] - n.fleet
+          not.na.group <- unique(not.na.group)
+        }
+      }
+      stop(paste(parameter[not.na.group + (n.groups - n.fleet), Group], 
+                 'are catching another fleet...set to NA \n', sep = ' '))
+    }
+  
+  
   }
-}
+  
+cat(paste(type, 'parameter file is functional'))
 }
 
 
