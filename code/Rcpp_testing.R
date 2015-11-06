899908aa86fad6741c40de90e6c599d4fae55edc
Sean Lucey <Sean.Lucey@NOAA.gov>
2015-11-06 16:00:27 -0500

    testing script

--- code/Rcpp_testing.R
+++ code/Rcpp_testing.R
@@ -16,6 +16,7 @@ int SplitSetPred(List stanzas, List state){
   NumericMatrix Age1           = as<NumericMatrix>(stanzas["Age1"]);
   NumericMatrix Age2           = as<NumericMatrix>(stanzas["Age2"]);
   NumericMatrix EcopathCode    = as<NumericMatrix>(stanzas["EcopathCode"]);
+  NumericMatrix stanzaBasePred = as<NumericMatrix>(stanzas["stanzaBasePred"]);
 
   //state parameters
   NumericVector state_BB = as<NumericVector>(state["BB"]);
@@ -41,6 +42,7 @@ int SplitSetPred(List stanzas, List state){
       }
       state_BB[ieco] = Bt;
       state_NN[ieco] = Nt;
+      stanzaBasePred(isp, ist) = pt;
     }
   }
   return(0);
@@ -50,7 +52,9 @@ int SplitSetPred(List stanzas, List state){
 SplitSetPred(REco.init$stanzas, REco.init$start_state)
 
 #Update numbers, weight, and biomass for multistanza groups
-deriv <- deriv_vector(REco.init$params, REco.init$start_state, REco.init$forcing,
+dydt0 <- deriv_vector(REco.init$params, REco.init$start_state, REco.init$forcing,
+                      REco.init$fishing, REco.init$stanzas, 0, 0, 0)
+dydt0 <- deriv_vector(REco.init$params, REco.init$start_state, REco.init$forcing,
                       REco.init$fishing, REco.init$stanzas, 1, 1, 0)
 
 cppFunction('
@@ -99,10 +103,13 @@ int update_stanzas(List stanzas, List state, List forcing, List deriv, int yr, i
     for(ist = 1; ist <= Nstanzas[isp]; ist++){
       ieco = EcopathCode(isp, ist);
       Su = exp(-LossPropToB[ieco] / STEPS_PER_YEAR / state_BB[ieco]);
-      Gf = FoodGain[ieco] / stanzaPred[ieco];
+      Gf = FoodGain[ieco] / stanzaPred(isp, ist);
+      //Rcpp::Rcout << Su << std::endl;
+      //Rcpp::Rcout << Gf << std::endl;
       for(ia = Age1(isp, ist); ia <= Age2(isp, ist); ia++){
         NageS(ia, isp) = NageS(ia, isp) * Su;
         WageS(ia, isp) = vBM[isp] * WageS(ia, isp) + Gf * SplitAlpha(ia, isp);
+        //Rcpp::Rcout << "WageS " << isp << " " << ia << " " << WageS(ia, isp) << std::endl;
         if(WageS(ia, isp) > Wmat[isp]){
           SpawnBio[isp] += NageS(ia, isp) * (WageS(ia, isp) - Wmat[isp]);
         };
@@ -111,6 +118,7 @@ int update_stanzas(List stanzas, List state, List forcing, List deriv, int yr, i
     EggsStanza[isp] = SpawnBio[isp] * SpawnEnergy[isp] * SpawnX[isp] /
                       (SpawnX[isp] - 1.0 + (SpawnBio[isp] / baseSpawnBio[isp]));
     EggsStanza[isp] *= force_byrecs(yr * STEPS_PER_YEAR + mon, ieco);
+
 
     // Need to add monthly recruitment
 
@@ -148,8 +156,97 @@ return(0);
 }
 ')
 
-update_stanzas(REco.init$stanzas, REco.init$start_state, REco.init$forcing, deriv, 
-               1, 1)
+
+
+
+deriv_vector <- function(params, state, forcing, fishing, stanzas, y, m, tt) {
+  .Call('Rpath_deriv_vector', PACKAGE = 'Rpath', params, state, forcing, fishing, stanzas, y, m, tt)
+}
+
+SplitSetPred <- function(stanzas, state) {
+  .Call('Rpath_SplitSetPred', PACKAGE = 'Rpath', stanzas, state)
+}
+
+SplitUpdate <- function(stanzas, state, forcing, deriv, yr, mon) {
+  .Call('Rpath_SplitUpdate', PACKAGE = 'Rpath', stanzas, state, forcing, deriv, yr, mon)
+}
+
+
+
+
+
+
+
+
+REco.init <- rsim.scenario(REco, juvfile, 100)
+STEPS_PER_YEAR <- 12
+SORWT <- 0.5
+DELTA_T <- 0.08333333
+#Steps in AB
+params <- REco.init$params
+instate <- REco.init$start_state
+forcing <- REco.init$forcing
+fishing <- REco.init$fishing
+stanzas <- REco.init$stanzas
+
+NoIntegrate <- params$NoIntegrate
+SplitSetPred(stanzas, instate)
+state <- copy(instate)
+dyt <- deriv_vector(params, state, forcing, fishing, stanzas, 0, 0, 0) 
+dd<-0
+for (y in 0:1){                                  
+  # Monthly loop                                     
+  for (m in 1:12){   
+    dd <- y * STEPS_PER_YEAR + m               
+    # Load old state and old derivative
+    old_BB <- state$BB 
+    old_Ftime <- state$Ftime
+    dydt0  <- dyt$DerivT
+    dyt   <- deriv_vector(params, state, forcing, fishing, stanzas, y, m, 0);
+    
+    # Extract needed parts of the derivative
+    dydt1       <- dyt$DerivT 
+    FoodGain    <- dyt$FoodGain					
+    biomeq      <- dyt$biomeq
+    FishingLoss <- dyt$FishingLoss  
+    
+    # Now Update the new State Biomass using Adams-Basforth
+    new_BB <- ifelse( NoIntegrate == 0,
+              (1.0 - SORWT) * biomeq + SORWT * old_BB,
+              ifelse( NoIntegrate > 0,
+                      old_BB + (DELTA_T / 2.0) * (3.0 * dydt1 - dydt0),
+                      old_BB))
+    
+    
+    pd <- ifelse(NoIntegrate < 0, stanzas$stanzaPred, old_BB)
+    new_Ftime <- ifelse((FoodGain > 0) & (pd > 0),
+                        0.1 + 0.9 * old_Ftime * ((1.0 - params$FtimeAdj) + 
+                                                   params$FtimeAdj * 
+                                                   params$FtimeQBOpt / 
+                                                   (FoodGain / pd)),
+                        old_Ftime)
+    
+    #Monthly Stanza (split pool) update
+    SplitUpdate(stanzas, state, forcing, dyt, y, m + 1)
+    SplitSetPred(stanzas, state)
+    
+    // Calculate catch assuming fixed Frate and exponential biomass change.
+    // kya 9/10/15 - replaced with linear, diff on monthly scale is minor
+    NumericVector new_CC = (DELTA_T * FishingLoss / old_BB) * 
+      (new_BB + old_BB) / 2.0;
+    // NumericVector new_CC = 
+      //               ifelse( new_BB==old_BB,
+                               //                 FishingLoss*DELTA_T,
+                               //                 (FishingLoss*DELTA_T/old_BB) *
+                                 //                   (new_BB-old_BB)/log(new_BB/old_BB) 
+                               //               );       
+    
+    // Set state to new values, including min/max traps
+    state["BB"]    = pmax(pmin(new_BB, B_BaseRef * BIGNUM), B_BaseRef * EPSILON);
+    state["Ftime"] = pmin(new_Ftime, 2.0);    
+    
+
+
 
 
 
