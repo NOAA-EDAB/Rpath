3d50f56bc90f5b08119aabd2be00850362ecb16c
Sean Lucey <Sean.Lucey@NOAA.gov>
2016-10-27 10:15:07 -0400

    Added code to automatically determine the width of the legend for a more streamlined figure

--- Rpath/R/ecosim_plot.R
+++ Rpath/R/ecosim_plot.R
@@ -31,8 +31,16 @@ rsim.plot <- function(Rsim.output, spname, indplot = F){
   ifelse(indplot, xmax <- length(biomass), xmax <- nrow(biomass))
   
   #Plot relative biomass
-  layout(matrix(c(1, 2), 1, 2), widths = c(4, 1))
   opar <- par(mar = c(4, 6, 2, 0))
+  
+  #Create space for legend
+  plot.new()
+  l <- legend(0, 0, bty='n', spname, 
+              plot=FALSE, fill = line.col, cex = 0.6)
+  # calculate right margin width in ndc
+  w <- grconvertX(l$rect$w, to='ndc') - grconvertX(0, to='ndc')
+  
+  par(omd=c(0, 1-w, 0, 1))
   plot(0, 0, ylim = c(ymin, ymax), xlim = c(0, xmax), 
        axes = F, xlab = '', ylab = '', type = 'n')
   axis(1)
@@ -47,9 +55,8 @@ rsim.plot <- function(Rsim.output, spname, indplot = F){
     if(indplot == F) lines(rel.bio[, i], col = line.col[i], lwd = 3)
   }
   
-  opar <- par(mar = c(0, 0, 0, 0))
-  plot(0, 0, xlab = '', ylab = '', axes = F)
-  legend('center', legend = spname, fill = line.col, cex = .6)
+  legend(par('usr')[2], par('usr')[4], bty='n', xpd=NA,
+         spname, fill = line.col, cex = 0.6)
   
   par(opar)
 }
