9b0ec5c4bfacf06f02d757fdd3dea228722fa6f8
kaydin <kerim@u.washington.edu>
2018-01-26 16:46:24 -0800

    working on fit read functions

--- Rpath/R/fitting.R
+++ Rpath/R/fitting.R
@@ -56,15 +56,12 @@ rsim.fit.obj <- function(scene,result){
 #########################################################
 #'@export
 read.rsim.fitting.catch <- function(insim,flist){
+
+  years <- as.numeric(rownames(insim$fishing$CATCH))
+  
   rsim <- insim
-  
-  if (is.null(rsim$fitting)){
-     rsim$fitting <- list()
-     #rsim$fitting <- list(years=years, NY=length(years) )
-  }  
-  
-  years <- as.numeric(rownames(scene$fishing$CATCH))
-  
+  if (is.null(rsim$fitting)){rsim$fitting <- list()}  
+
   CATCH <- NULL
   for (f in flist){
       cdat  <- read.csv(f)
@@ -74,6 +71,7 @@ read.rsim.fitting.catch <- function(insim,flist){
       wt   <- rep(1,length(sdat$x))
       CATCH <- rbind(CATCH, cbind(sdat,sd,wt))
   }
+  
   rsim$fitting$catch <- CATCH
   colnames(rsim$fitting$catch) <- c("year","species","obs","sd","wt")
   
@@ -84,3 +82,92 @@ read.rsim.fitting.catch <- function(insim,flist){
                            length(rsim$fitting$catch$year),2)] <- rsim$fitting$catch$obs 
   return(rsim)
 }
+
+#########################################################
+#'@export
+read.rsim.fitting.biomass <- function(insim,flist){
+
+  years <- as.numeric(rownames(insim$fishing$CATCH))
+  
+  rsim <- insim
+  if (is.null(rsim$fitting)){rsim$fitting <- list()}  
+  
+  BIO <- NULL
+  for (f in flist){
+    cdat  <- read.csv(f)
+    ccdat <-cdat[!is.na(cdat$AMOUNT)& cdat$YEAR %in% years ,]        
+    wt   <- rep(1,length(ccdat$YEAR))
+    type <- as.character(rep("absolute",length(ccdat$YEAR)))
+    BIO <- rbind(BIO, data.frame(as.numeric(ccdat$YEAR),as.character(ccdat$SPECIES),
+                                     as.numeric(ccdat$AMOUNT/ccdat$SCALE),as.numeric(ccdat$SD),as.numeric(wt),as.character(type),
+                                     stringsAsFactors = F))
+  }
+  
+  rsim.fitting.biomass <- BIO
+  colnames(rsim$fitting$biomass) <- c("year","species","obs","sd","wt","type")  
+    
+  return(rsim)
+}
+
+#########################################################
+#'@export  
+read.rsim.fitting.diet <- function(insim,flist){
+  
+  years <- as.numeric(rownames(insim$fishing$CATCH))
+  
+  rsim <- insim
+  if (is.null(rsim$fitting)){rsim$fitting <- list()}  
+
+  QDAT <- NULL
+  for (f in flist){
+    rdat <- read.csv(f)
+    ddat <- rdat[rdat$year %in% years,]
+    QDAT <- rbind(QDAT, data.frame(ddat$year,ddat$pred,ddat$cperwMean,ddat$cperwSD,rep(1,length(ddat[,1]))))
+  }
+
+  rsim$fitting$ration <- QDAT
+  colnames(rsim$fitting$ration)<-c("year","species","obs","sd","wt")  
+  
+  rsim$fitting$diet <- read_diet_alphas(insim,flist)
+  
+  return(rsim)
+}
+  
+#########################################################
+
+read_diet_alphas<-function(insim,flist){
+  # Implements log-likelihood from Ainsworth et al. 2010 (Ecol. App. 20: 2188-2202)
+  # LogLiklihood = lgamma(alpha0) - sum(lgamma(alpha)) + sum( (alpha-1)*P )
+  # lgamma(alpha0) is split over other summed lines to make summing easier
+  odat <- NULL
+  for (f in flist){  
+    ddat <- read.csv(f)
+    fpy  <- 1+which(colnames(ddat)=="alpha0")
+    lpy  <- length(colnames(ddat))
+    pred <- ddat$pred
+    prey <- as.character(t(matrix(colnames(ddat)[fpy:lpy],length(colnames(ddat)[fpy:lpy]),length(pred))))
+    year <- ddat$year
+    almat  <- ddat[,fpy:lpy]
+    alpha0 <- rowSums(almat)
+    alprop <- as.numeric(unlist(almat/alpha0))
+    log_a0part <- alprop*lgamma(alpha0)
+    
+    alpha     <- as.numeric(unlist(ddat[,fpy:lpy]))
+    log_alpha <- lgamma(alpha) 
+    log_diff  <- log_a0part-log_alpha
+    alphaM1   <- alpha-1
+    wt <- rep(1,length(alphaM1))     
+    odat<-rbind(odat,data.frame(year,pred,prey,alpha,log_alpha,log_a0part,log_diff,alphaM1,alprop,wt))
+  }
+  dalph  <- odat[order(odat$pred,odat$year),]
+  linkprey <- insim$params$spname[insim$params$PreyFrom+1]
+  linkpred <- insim$params$spname[insim$params$PreyTo+1]
+  simlink <- rep(NA,length(dalph[,1]))
+  for (i in 1:(length(dalph[,1]))){
+    simlink[i]<-which(linkprey==dalph$prey[i] & linkpred==dalph$pred[i] )     
+  }
+  return(data.frame(dalph,simlink))
+}  
+
+#########################################################
+
