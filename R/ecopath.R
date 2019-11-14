f59e92fd7f0961a803fc0b0caff75e3162153519
Sean Lucey <Sean.Lucey@NOAA.gov>
2019-11-14 12:19:18 -0500

    Added mixotroph fix from privite repository

--- R/ecopath.R
+++ R/ecopath.R
@@ -37,12 +37,12 @@ rpath <- function(Rpath.params, eco.name = NA, eco.area = 1){
   if(sapply(diet, class)[1] == 'character') diet[, 1 := NULL]
 
   #Adjust diet comp of mixotrophs
-  #mixotrophs <- which(model[, Type] > 0 & model[, Type] < 1)
-  #mix.Q <- 1 - model[mixotrophs, Type]
-  # for(i in seq_along(mixotrophs)){
-  #   new.dc <- diet[, mixotrophs[i], with = F] * mix.Q[i]
-  #   diet[, mixotrophs[i] := new.dc, with = F]
-  # }
+  mixotrophs <- which(model[, Type] > 0 & model[, Type] < 1)
+  mix.Q <- 1 - model[mixotrophs, Type]
+  for(i in seq_along(mixotrophs)){
+    new.dc <- diet[, mixotrophs[i], with = F] * mix.Q[i]
+    diet[, mixotrophs[i] := new.dc]
+  }
   
   #Convert NAs to zero in diet matrix
   diet[is.na(diet)] <- 0
@@ -176,11 +176,11 @@ rpath <- function(Rpath.params, eco.name = NA, eco.area = 1){
   dietplus <- as.matrix(diet)
   dimnames(dietplus) <- list(NULL, NULL)
   #Adjust for mixotrophs (partial primary producers)
-  mixotrophs <- which(model[, Type] > 0 & model[, Type] < 1)
-  mix.Q <- 1 - model[mixotrophs, Type]
-  for(i in seq_along(mixotrophs)){
-    dietplus[, mixotrophs[i]] <- dietplus[, mixotrophs[i]] * mix.Q[i]
-  }
+  # mixotrophs <- which(model[, Type] > 0 & model[, Type] < 1)
+  # mix.Q <- 1 - model[mixotrophs, Type]
+  # for(i in seq_along(mixotrophs)){
+  #   dietplus[, mixotrophs[i]] <- dietplus[, mixotrophs[i]] * mix.Q[i]
+  # }
   #Adjust for diet import (Consumption outside model)
   import <- which(dietplus[nrow(diet), ] > 0)
   for(i in seq_along(import)){
