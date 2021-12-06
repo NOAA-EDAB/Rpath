4cb046102057ef9d521f3c2cfd207c618db2019e
Sean Lucey <Sean.Lucey@NOAA.gov>
2021-12-06 11:37:23 -0500

    Added R Ecosystem

--- R/Data.R
+++ R/Data.R
@@ -18,3 +18,23 @@
 #'
 #'
 "AB.params"
+
+#' Rpath parameter set for the fictitious R Ecosystem
+#'
+#' This is a parameter set for the example fictitious ecosystem of R Ecosystem.
+#' R Ecosystem is used as an introduction to Rpath.
+#' 
+#' @format An Rpath.params object
+#' @family rpathParameters
+#'
+#' \describe{
+#'\item{model}{Main model parameters including Biomass, PB, QB, EE, etc.}
+#'\item{diet}{Diet composition matrix with predators as columns and prey as rows}
+#'\item{stanzas}{List object containing necessary parameters for multistanza groups}
+#'\item{pedigree}{Matrix of certainty surrounding parameter values.  Defaulted to 1
+#'      for this fictitious system.}
+#'
+#' }
+#'
+#'
+"REco.params"
