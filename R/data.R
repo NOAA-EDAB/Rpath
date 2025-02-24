350d9b4ae0f581b9b24150dd8692cbb097c46632
Andy Whitehouse <andy.whitehouse@noaa.gov>
2025-02-24 10:09:42 -0800

    Fixed line length for the example ecosystems in the data.R file.

--- R/data.R
+++ R/data.R
@@ -2,17 +2,21 @@
 
 #' AB.params
 #'
-#' Anchovy Bay (\code{AB.params}) is a sample ecosystem frequently used as an example ecosystem with EwE (see Christensen and Walters, 2024).
+#' Anchovy Bay (\code{AB.params}) is a sample ecosystem frequently used as an
+#' example ecosystem with EwE (see Christensen and Walters, 2024).
 #'
 #' @format An unbalanced Rpath model object that contains:
 #' \describe{
-#'   \item{model}{A data.table containing Ecopath unbalanced ecosystem parameters (base parameters and fisheries).}
+#'   \item{model}{A data.table containing Ecopath unbalanced ecosystem
+#'   parameters (base parameters and fisheries).}
 #'   \item{diet}{A data.table containing the Ecopath model's diet matrix.}
 #'   \item{stanzas}{Parameters for multistanza groups.}
-#'   \item{pedigree}{A data.table containing the data quality (pedigree) for the Ecopath model.}   
+#'   \item{pedigree}{A data.table containing the data quality (pedigree) for the
+#'   Ecopath model.}
 #'   ...
 #' }
-#' @source Christensen V, Walters CJ (2024) Ecosystem Modelling with EwE. The University of British Columbia, Vancouver, BC. doi:10.14288/24d7-ab68
+#' @source Christensen V, Walters CJ (2024) Ecosystem Modelling with EwE. The
+#'   University of British Columbia, Vancouver, BC. doi:10.14288/24d7-ab68
 #' @examples
 #' # Balance the model
 #' AB_bal <- rpath(AB.params)
@@ -25,14 +29,19 @@
 #'
 #' @format An unbalanced Rpath model object that contains:
 #' \describe{
-#'   \item{model}{A data.table containing Ecopath unbalanced ecosystem parameters (base parameters and fisheries).}
+#'   \item{model}{A data.table containing Ecopath unbalanced ecosystem
+#'   parameters (base parameters and fisheries).}
 #'   \item{diet}{A data.table containing the Ecopath model's diet matrix.}
 #'   \item{stanzas}{Parameters for multistanza groups.}
-#'   \item{pedigree}{A data.table containing the data quality (pedigree) for the Ecopath model.}
+#'   \item{pedigree}{A data.table containing the data quality (pedigree) for the
+#'   Ecopath model.}
 #'   ...
 #' }
 #' @seealso \code{vignette("ModelSetup", package = "Rpath")} for an example
 #' of how to build this tutorial ecosystem.
+#' @references Lucey SM, Gaichas SK, Aydin KY (2020) Conducting reproducible
+#'   ecosystem modeling using the open source mass balance model Rpath. Ecol
+#'   Model 427:11. doi:10.1016/j.ecolmodel.2020.109057
 #' @examples
 #' # Balance the model
 #' REco_bal <- rpath(REco.params)
@@ -46,15 +55,23 @@
 #'
 #' @format An unbalanced Rpath model object that contains:
 #' \describe{
-#'   \item{model}{A data.table containing Ecopath unbalanced ecosystem parameters (base parameters and fisheries).}
+#'   \item{model}{A data.table containing Ecopath unbalanced ecosystem
+#'   parameters (base parameters and fisheries).}
 #'   \item{diet}{A data.table containing the Ecopath model's diet matrix.}
 #'   \item{stanzas}{Parameters for multistanza groups.}
-#'   \item{pedigree}{A data.table containing the data quality (pedigree) for the Ecopath model.}   
+#'   \item{pedigree}{A data.table containing the data quality (pedigree) for the
+#'   Ecopath model.}
 #'   ...
 #' }
-#' @note This is an aggregated version of the 1990s eastern Bering Sea Ecopath model of Aydin et al. (2007) and does not include stanzas.
-#' @source Whitehouse and Aydin 2020.  Assessing the sensitivity of three Alaska marine food webs to perturbations: an example of Ecosim simulations using Rpath. https://doi.org/10.1016/j.ecolmodel.2020.109074  
-#' @references Aydin KY, Gaichas S, Ortiz I, Kinzey D, Friday N (2007) A comparison of the Bering Sea, Gulf of Alaska, and Aleutian Islands large marine ecosystems through food web modeling. U.S. Dep. Commer., NOAA Tech. Memo. NMFS-AFSC-178. 
+#' @note This is an aggregated version of the 1990s eastern Bering Sea Ecopath
+#'   model of Aydin et al. (2007) and does not include stanzas.
+#' @source Whitehouse and Aydin 2020.  Assessing the sensitivity of three Alaska
+#'   marine food webs to perturbations: an example of Ecosim simulations using
+#'   Rpath. https://doi.org/10.1016/j.ecolmodel.2020.109074
+#' @references Aydin KY, Gaichas S, Ortiz I, Kinzey D, Friday N (2007) A
+#'   comparison of the Bering Sea, Gulf of Alaska, and Aleutian Islands large
+#'   marine ecosystems through food web modeling. U.S. Dep. Commer., NOAA Tech.
+#'   Memo. NMFS-AFSC-178.
 #' @examples
 #' # Balance the model
 #' ebs_bal <- rpath(Ecosense.EBS)
@@ -68,15 +85,23 @@
 #'
 #' @format An unbalanced Rpath model object that contains:
 #' \describe{
-#'   \item{model}{A data.table containing Ecopath unbalanced ecosystem parameters (base parameters and fisheries).}
+#'   \item{model}{A data.table containing Ecopath unbalanced ecosystem
+#'   parameters (base parameters and fisheries).}
 #'   \item{diet}{A data.table containing the Ecopath model's diet matrix.}
 #'   \item{stanzas}{Parameters for multistanza groups.}
-#'   \item{pedigree}{A data.table containing the data quality (pedigree) for the Ecopath model.}   
+#'   \item{pedigree}{A data.table containing the data quality (pedigree) for the
+#'   Ecopath model.}
 #'   ...
 #' }
-#' @note This is an aggregated version of the 1990s Gulf of Alaska Ecopath model of Aydin et al. (2007) and does not include stanzas.
-#' @source Whitehouse and Aydin 2020.  Assessing the sensitivity of three Alaska marine food webs to perturbations: an example of Ecosim simulations using Rpath. https://doi.org/10.1016/j.ecolmodel.2020.109074  
-#' @references Aydin KY, Gaichas S, Ortiz I, Kinzey D, Friday N (2007) A comparison of the Bering Sea, Gulf of Alaska, and Aleutian Islands large marine ecosystems through food web modeling. U.S. Dep. Commer., NOAA Tech. Memo. NMFS-AFSC-178. 
+#' @note This is an aggregated version of the 1990s Gulf of Alaska Ecopath model
+#'   of Aydin et al. (2007) and does not include stanzas.
+#' @source Whitehouse and Aydin 2020.  Assessing the sensitivity of three Alaska
+#'   marine food webs to perturbations: an example of Ecosim simulations using
+#'   Rpath. https://doi.org/10.1016/j.ecolmodel.2020.109074
+#' @references Aydin KY, Gaichas S, Ortiz I, Kinzey D, Friday N (2007) A
+#'   comparison of the Bering Sea, Gulf of Alaska, and Aleutian Islands large
+#'   marine ecosystems through food web modeling. U.S. Dep. Commer., NOAA Tech.
+#'   Memo. NMFS-AFSC-178.
 #' @examples
 #' # Balance the model
 #' goa_bal <- rpath(Ecosense.GOA)
@@ -90,15 +115,22 @@
 #'
 #' @format An unbalanced Rpath model object that contains:
 #' \describe{
-#'   \item{model}{A data.table containing Ecopath unbalanced ecosystem parameters (base parameters and fisheries).}
+#'   \item{model}{A data.table containing Ecopath unbalanced ecosystem
+#'   parameters (base parameters and fisheries).}
 #'   \item{diet}{A data.table containing the Ecopath model's diet matrix.}
 #'   \item{stanzas}{Parameters for multistanza groups.}
-#'   \item{pedigree}{A data.table containing the data quality (pedigree) for the Ecopath model.}   
+#'   \item{pedigree}{A data.table containing the data quality (pedigree) for the
+#'   Ecopath model.}
 #'   ...   
 #' }
-#' @note This is an aggregated version of the eastern Chukchi Sea Ecopath model of Whitehouse and Aydin (2016).
-#' @source Whitehouse and Aydin 2020.  Assessing the sensitivity of three Alaska marine food webs to perturbations: an example of Ecosim simulations using Rpath. https://doi.org/10.1016/j.ecolmodel.2020.109074  
-#' @references Whitehouse GA, Aydin KY (2016) Trophic structure of the eastern Chukchi Sea: An updated mass balance food web model. U.S. Dep Commer, NOAA Tech. Memo. NMFS-AFSC-318. doi:10.7289/V5/TM-AFSC-318 
+#' @note This is an aggregated version of the eastern Chukchi Sea Ecopath model
+#'   of Whitehouse and Aydin (2016).
+#' @source Whitehouse and Aydin 2020.  Assessing the sensitivity of three Alaska
+#'   marine food webs to perturbations: an example of Ecosim simulations using
+#'   Rpath. https://doi.org/10.1016/j.ecolmodel.2020.109074
+#' @references Whitehouse GA, Aydin KY (2016) Trophic structure of the eastern
+#'   Chukchi Sea: An updated mass balance food web model. U.S. Dep Commer, NOAA
+#'   Tech. Memo. NMFS-AFSC-318. doi:10.7289/V5/TM-AFSC-318
 #' @examples
 #' # Balance the model
 #' ecs_bal <- rpath(Ecosense.ECS)
