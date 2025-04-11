9650ede13232e28cd8280501f2a8c86a45c3f12b
Bia Dias <bia.dias@noaa.gov>
2025-04-11 12:41:05 -0600

    Updating the Hex function.

--- data-raw/create_hex.R
+++ data-raw/create_hex.R
@@ -7,9 +7,9 @@
 #' @param exportPath Path to location of hex image. Final hex should be saved in man/figures folde
 #' 
 #' @return png file. r
-#'
+#' this function is highly costume for the current hex image. Please do not change it. 
 
-create_hex <- function(imgurl = NULL, exportPath = here::here("man/figures")) {
+create_hex <- function(imgurl = NULL, exportPath = here::here("man/figures"), hex_color= "#062825", hex_border="#083F22") {
   
   if(is.null(imgurl)){
     stop("Please provide a path to an image file")
@@ -18,27 +18,29 @@ create_hex <- function(imgurl = NULL, exportPath = here::here("man/figures")) {
   hexSticker::sticker(imgurl,
                       package = NULL,
                       p_size=24,
-                      s_x=1,
+                      s_x=0.99,
                       s_y=1,
-                      s_width=1.28,
+                      s_width = 1,
+                      s_height = 1,
                       p_x = 1,
                       p_y = 1.2,
-                      h_size = 1,
-                      h_fill="#FFFFFF",
-                      h_color="#bc4700",
+                      h_size = 1.5,
+                      h_fill= hex_color,
+                      h_color= hex_border,     #"#bc4700", "#083F22"
                       #angle = 30,
                       spotlight=T,
-                      l_x = 1.5,
-                      l_y = 1.1,
-                      l_width = 3,
-                      l_height = 3,
-                      l_alpha = 0.5,
-                      u_x = .99,
-                      u_y=.05,
+                      l_x = 1,
+                      l_y = 1.75,
+                      l_width = 1.5,
+                      l_height = 1.5,
+                      l_alpha = 0.15,
+                      u_x = 1.78,
+                      u_y=.7,
                       url = "noaa-edab.github.io/Rpath",
-                      u_size = 5.5,
-                      u_color = "#FFFFFF",
-                      white_around_sticker = T,
+                      u_size = 5,
+                      u_color = "#9DF2EF",
+                      u_angle = 90,
+                      white_around_sticker = FALSE,
                       #filename = here::here("man/figures", "logo.png"),
                       #filename = here::here("data-raw", "logo.png")
                       filename = paste0(exportPath, "/logo.png")
