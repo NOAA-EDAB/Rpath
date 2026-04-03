bdd65f9a07d2da1825ffc188831824e210ef3fa2
kerim aydin <kerim@uw>
2026-04-03 14:24:35 -0700

    biomass index was off by 1 on R side

--- R/ecosim.R
+++ R/ecosim.R
@@ -183,7 +183,7 @@ rsim.run <- function(Rsim.scenario, method = 'RK4', years = 1:100, spname = NULL
                                         "FoodLoss", "UnAssimLoss", "ActiveRespLoss",
                                         "MzeroLoss", "FishingLoss", "DetritalLoss", "MigrateLoss")
   # Divide rates by biomass, with offset of 1 for beginning of timestep biomass not end of timestep
-  StartBio <- c(as.numeric(scene$start_state$Biomass[sp.num]), head(rout$out_Biomass[,sp.num],-1))
+  StartBio <- c(as.numeric(scene$start_state$Biomass[sp.num+1]), head(rout$out_Biomass[,sp.num+1],-1))
   rout$out_species_rates <- cbind(StartBio, rout$out_species_rates[,1:10]/StartBio)
   #rout$out_species_rates[,1:10] <- rout$out_species_rates[,1:10]/rout$out_species_rates$biomass
   
